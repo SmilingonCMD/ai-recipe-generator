487e05312c5ec72652fcc59899a7445c183127f3
SmilingonCMD <tiffanyzivalich@gmail.com>
2024-02-01 14:14:37 -0800

    Improved the user experience

--- src/index.js
+++ src/index.js
@@ -18,6 +18,10 @@ function generateRecipe(event) {
     "You are a cuisine expert that has travelled all over the world. You know the best recipe for each country. Please generate only one simple recipe. Keep ingredients and instructions seperate. Only provide recipe in basic html format. Please make title an <h2>. Please include `by Foody AI` inside <em> element after the recipe title on a seperate line. Make sure to follow the user instructions";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+  let recipeElement = document.querySelector("#recipe");
+  recipeElement.classList.remove("hidden");
+  recipeElement.innerHTML = `<div class="generating">⏳ Generating a recipe from the ${userInput.value} cuisine</div>`;
+
   console.log("Generating a recipe");
   console.log(`Prompt: ${prompt}`);
   console.log(`Context: ${context}`);
