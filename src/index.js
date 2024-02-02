983e4ff4cc464e1dbae32e693b99c58df53e2036
SmilingonCMD <tiffanyzivalich@gmail.com>
2024-02-02 14:52:52 -0800

    Styled project with new fonts, colors, and images

--- src/index.js
+++ src/index.js
@@ -13,14 +13,14 @@ function generateRecipe(event) {
   let userInput = document.querySelector("#user-input");
 
   let apiKey = "93ffacd3347f947bo5021d6t7b18ceab";
-  let prompt = `User instructions are: Generate a short and simple recipe with no more than five ingredients and is from ${userInput.value} cuisine`;
+  let prompt = `User instructions are: Generate a short and easy dessert recipe with no more than four ingredients and no more than six lines of instruction and is from ${userInput.value}`;
   let context =
-    "You are a cuisine expert that has travelled all over the world. You know the best recipe for each country. Please generate only one simple recipe. Keep ingredients and instructions seperate. Only provide recipe in basic html format. Please make title an <h2>. Please include `by Foody AI` inside <em> element after the recipe title on a seperate line. Make sure to follow the user instructions";
+    "You are a cuisine expert that has travelled all over the world. You know the best recipe for each country. Please generate only one simple recipe. Only provide recipe in basic html format. Please make title an <h2>. Please include `by Pâtisserie Perry` inside <em> element after the recipe title on a seperate line. Make sure to follow the user instructions";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   let recipeElement = document.querySelector("#recipe");
   recipeElement.classList.remove("hidden");
-  recipeElement.innerHTML = `<div class="generating">⏳ Generating a recipe from the ${userInput.value} cuisine</div>`;
+  recipeElement.innerHTML = `<div class="generating">⏳ Generating the best dessert recipe from ${userInput.value}</div>`;
 
   axios.get(apiUrl).then(displayRecipe);
 }
